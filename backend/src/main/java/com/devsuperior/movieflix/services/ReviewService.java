12d2907d41f980a91b8cd0c8fd49b13e7d12da11
Rafaelxndre <rafaelxndre@gmail.com>
2022-05-01 21:16:07 -0300

    New Review, com retorno dos dados do membro que fez o comentario

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.repositories.UserRepository;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private ReviewRepository repository;
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Transactional
+	public ReviewDTO insert(ReviewDTO dto) {
+		Review review = new Review();
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		User userAuthenticated = userRepository.findByEmail(authentication.getName());
+		review.setUser(userAuthenticated);
+		
+		review.setText(dto.getText());
+		
+		Movie movie = movieRepository.getOne(dto.getMovieId());
+		review.setMovie(movie);		
+		
+		review = repository.save(review);		
+		return new ReviewDTO(review);
+	}
+}
